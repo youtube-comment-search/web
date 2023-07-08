200785a71353afbb7abcbd981d2ad5fb5e134511
YoungHwa Byun <rlaalsdl111@gmail.com>
2023-07-09 01:47:35 +0900

    :wrench: Prettier config 추가
    - #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="App">@Youtube Comment Search</div>;
+  return <div className="App">@Youtube Comment Search</div>
 }
 
-export default App;
+export default App
