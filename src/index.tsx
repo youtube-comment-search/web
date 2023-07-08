a34ec4af9aae3054b535bf7aff7d1310875c7d6a
YoungHwa Byun <rlaalsdl111@gmail.com>
2023-07-09 01:45:34 +0900

    :package: Add msw
    - #1

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BASE_URL } from './constants'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function main() {
+  if (process.env.REACT_APP_ENV === 'msw') {
+    if (window.location.pathname === BASE_URL) {
+      window.location.pathname += '/'
+      return
+    }
+
+    const { worker } = require('./msw/worker')
+
+    await worker.start({
+      serviceWorker: {
+        url: BASE_URL + '/mockServiceWorker.js',
+      },
+    })
+  }
+
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement,
+  )
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
+main()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
